bf2d0b172aea79ece17b2fd8d37ffe735dc4e9c1
Kacper Agatowski <n00212272@iadt.ie>
2023-10-30 18:20:29 +0000

    slighly updated nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,23 @@
+import {Nav, Container, NavDropdown, Form, Button } from 'react-bootstrap';
+
+
 import {Link} from 'react-router-dom';
 
 const Navbar = () => {
+
     return (
+        <>
+        <Container fluid className='bg-black' >
         <h1><Link to='/'>Home</Link></h1>
+        
+        <Nav
+        className="me-auto my-2 my-lg-0"
+        style={{maxHeight: '100px'}}
+        navbarScroll>
+            </Nav>
+           
+            </Container>
+        </>
     );
 }
 
