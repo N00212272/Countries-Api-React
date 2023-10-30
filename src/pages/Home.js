6a5bcf7f1ebca7bc49baf8ce81646100cd431886
Kacper Agatowski <n00212272@iadt.ie>
2023-10-30 15:36:49 +0000

    created a search to search countries

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,54 @@
 import {useState , useEffect} from 'react';
 import axios from 'axios';
 import CountryCard from '../components/CountryCard';
-import {Row} from 'react-bootstrap';
+import {Row, Button} from 'react-bootstrap';
 
 const Home = () => {
     const [countriesList, setCountriesList] = useState([]);
+    const [term, setTerm] = useState("");
 
+    const handleChange = (e) => {
+        setTerm(e.target.value)
+     };
+
+     const handleClick = () => {
+        searchCountry();
+     }
+
+     const handleKeyUp = (e) => {
+         if(e.key === 'Enter'){
+            searchCountry();
+         }
+         
+     }
+
+     const searchCountry = () => {
+
+         if(!term){
+             alert("please enter a search term");
+             return;
+         }
+
+         axios.get(`https://restcountries.com/v3.1/name/${term}`)
+        
+         .then((response) => {
+            console.log(response.data);
+            setCountriesList(response.data);
+
+         })
+
+         .catch((error) => {
+
+             console.log(error);
+
+         });
+
+         setTerm("")
+
+     }
     useEffect(()=>{
         axios.get('https://restcountries.com/v3.1/all').then(response => {
-            console.log(response.data);
+            // console.log(response.data);
             setCountriesList(response.data);
         }).catch(error => {
             console.error(error);
@@ -20,10 +60,15 @@ const Home = () => {
     });
 
     return (
+        <>
+        <div className="search">
+        <input type="text" value={term} onChange={handleChange} onKeyUp={handleKeyUp} />
+        <Button variant="primary" onClick={handleClick}>Search</Button>
+    </div>
         <Row className= "g-2" md={3} xs={1} >
         {countryCards}
         </Row>
-        
+        </>
     );
 }
 
