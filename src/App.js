2787ad8bd32e1795770addf27d47d9b5b1c7d5dd
Kacper Agatowski <n00212272@iadt.ie>
2023-11-02 14:00:04 +0000

    used parent to pass from search to another child

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {Container, Row , Col} from "react-bootstrap";
+
 import './assets/app.css';
 
 //import pages
@@ -9,15 +10,26 @@ import SingleCountry from "./pages/SingleCountry";
 //import components
 import Navbar from "./components/Navbar";
 
+import {useState , useEffect} from 'react';
+
 function App() {
+
+  const [term, setTerm] = useState("");
+  
+  const handleChange = (e) => {
+    setTerm(e.target.value);
+}
+  // const handleSelect = (selected) = {
+
+  // }
   return(
     <Router>
-       <Navbar />
+       <Navbar handleChange={handleChange} term={term}/>
       <Container>
         <Row>
           <Col>
             <Routes>
-            <Route path='/' element={<Home/>} />
+            <Route path='/' element={<Home term={term}/>} />
             <Route path='/country/:name' element={<SingleCountry/>} />
             </Routes>
           </Col>
