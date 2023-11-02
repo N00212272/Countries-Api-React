36320893ce6cb4544ccc76f17913fc5215ff99e8
Kacper Agatowski <n00212272@iadt.ie>
2023-11-02 14:01:42 +0000

    tidied code to make it look neater

--- src/pages/SingleCountry.js
+++ src/pages/SingleCountry.js
@@ -3,6 +3,8 @@ import {useState, useEffect} from "react";
 import axios from "axios";
 import {Row,Col, Spinner, Image, Carousel, Container} from 'react-bootstrap';
 import CountryCard from '../components/CountryCard';
+import WeatherCard from "../components/WeatherCard";
+
 
 const SingleCountry = () => {
   let { name } = useParams();
@@ -11,16 +13,17 @@ const SingleCountry = () => {
 
   const [borders, setBorders] = useState([]);
 
+  
+
   useEffect(() => {
     axios
       .get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
       .then((response) => {
         const countryData = response.data[0];
         setCountry(countryData);
-
+       
         // Some countries didnt have borders and the page would crash on some countries
-        if (countryData.borders.length > 0) {
-          
+       
           axios
             .get(
                 `https://restcountries.com/v3.1/alpha?codes=${countryData.borders}`
@@ -30,71 +33,85 @@ const SingleCountry = () => {
             })
             .catch((error) => {
               console.error(error);
-            });
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+            })
+    
+          .catch((error) => {
+            console.error(error);
+          });
+      }) 
+      
   }, [name]);
+  
 
 
   if (!country) {
     return <Spinner animation="border" />;
   }
 
+
   return (
     <Container>
     <Row className="mt-5">
-      <Col>
-      <h1>
+      <Col xs={12} lg={6} pt={5}>
+      <h2>
           <b>Common name: </b>
           {country.name.common}
-        </h1>
-        <Image className="border border-black" height="500" width="750" src={country.flags.png} />
+        </h2>
+        <Image className="border border-black" width="600" height="300"  src={country.flags.png} />
       </Col>
-      <Col>
-     
-        <p>
+      
+      <Col xs={12} lg={6}>
+        <h2 className="mb-3"><b>Country Details</b></h2>
+        <h5>
           <b>Official name: </b>
           {country.name.official}
-        </p>
-        <p>
+        </h5>
+        <h5>
           <b>Region: </b>
           {country.region}
-        </p>
-        <p>
+        </h5>
+        <h5>
           <b>Sub Region: </b>
           {country.subregion}
-        </p>
-        <p>
+        </h5>
+        <h5>
           <b>Population: </b>
           {country.population}
-        </p>
-        <p>
+        </h5>
+        <h5>
           <b>Currencies: </b>
           {Object.values(country.currencies)[0].name}
-        </p>
+        </h5>
         </Col>
-        <Col>
+        
+        <Col xs={12} lg={4} >
         {borders.length > 0 && (
           <div>
-            <b>Bordering Countries: </b>
+            <h5 className="mt-5"><b>Bordering Countries: </b></h5>
+            
             {/* added Carousel as some countries had to many border and looked messy */}
             <Carousel>
               {borders.map((border, i) => (
                  <Carousel.Item>
                 <div key={i}>
-                  <CountryCard flag={border.flags.png} name={border.name.common} />
+                  <CountryCard flag={border.flags.png} name={border.name.common} region={border.region} />
                 </div>
                 </Carousel.Item>
               ))}
             </Carousel>
           </div>
         )}
+        {borders.length === 0 && (
+          <h3 className="mt-3">I am an island!</h3>
+        )}
         </Col>
-    
-    </Row>
+        <Col xs={12} lg={4} className="mt-5">
+          <WeatherCard  name={name}/>
+        </Col>
+
+
+        </Row>
+        
     </Container>
     
   );
