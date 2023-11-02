b59d744a7cfb41ea490a61545a4afd5ee41c1030
Kacper Agatowski <n00212272@iadt.ie>
2023-11-02 14:00:24 +0000

    created a search and started region dropdown

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,41 @@
-import {Nav, Container, NavDropdown, Form, Button } from 'react-bootstrap';
+import {Nav, Container, NavDropdown, Form, Button, Dropdown } from 'react-bootstrap';
+import {useState , useEffect} from 'react';
+
+import {Link, useNavigate} from 'react-router-dom';
+
+const Navbar = (props) => {
 
 
-import {Link} from 'react-router-dom';
-
-const Navbar = () => {
-
+   
+    let navigate = useNavigate();
+    const handleInputChange = (e) => {
+        navigate('/');
+        props.handleChange(e);
+    }
     return (
         <>
         <Container fluid className='bg-black' >
-        <h1><Link to='/'>Home</Link></h1>
         
         <Nav
         className="me-auto my-2 my-lg-0"
         style={{maxHeight: '100px'}}
         navbarScroll>
-            </Nav>
+
+        <h1><Link to='/'>Home</Link></h1>
+        <input type='text' onChange={handleInputChange} value={props.term}/>
+        <Dropdown>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+        Region
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">All</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Africa</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Europe</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Asia</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+        </Nav>
            
             </Container>
         </>
