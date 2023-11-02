c0305c82a6c9644ed20ef6190fc406255d841387
Kacper Agatowski <n00212272@iadt.ie>
2023-11-02 14:01:03 +0000

    moved search from home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,76 +4,50 @@ import CountryCard from '../components/CountryCard';
 import {Row, Button} from 'react-bootstrap';
 
 
-const Home = () => {
+const Home = (props) => {
     
     const [countriesList, setCountriesList] = useState([]);
-    const [term, setTerm] = useState("");
+   
+     const [searchCountries, setSearchCountries] = useState([]);
 
-    ;
+  
+    
 
-    const handleChange = (e) => {
-        setTerm(e.target.value)
-     };
+     useEffect(() => {
+      
+        if(props.term <=3){
+            setSearchCountries(countriesList)
+        }
+        else{
+            let countryFilter = countriesList.filter((country) => {
+                return country.name.common.toLowerCase().includes(props.term.toLowerCase());
+            })
+            setSearchCountries(countryFilter)
+        }
+    },[countriesList,props.term]);
+      
 
-     const handleClick = () => {
-        searchCountry();
-     }
 
-     const handleKeyUp = (e) => {
-         if(e.key === 'Enter'){
-            searchCountry();
-         }
-         
-     }
-
-     const searchCountry = () => {
-
-         if(!term){
-             alert("please enter a search term");
-             return;
-         }
-
-         axios.get(`https://restcountries.com/v3.1/name/${term}`)
-        
-         .then((response) => {
-            console.log(response.data);
-            setCountriesList(response.data);
-
-         })
-
-         .catch((error) => {
-
-             console.log(error);
-
-         });
-
-         setTerm("")
-
-     }
     useEffect(()=>{
         axios.get('https://restcountries.com/v3.1/all').then(response => {
             // console.log(response.data);
             setCountriesList(response.data);
+            setSearchCountries(response.data);
         }).catch(error => {
             console.error(error);
         });
-    }, [])
+    }, []);
 
-    let countryCards = countriesList.map((country, i) => {
+    
+
+    let countryCards = searchCountries.map((country, i) => {
         return <CountryCard key={i} flag={country.flags.png} name={country.name.common} region={country.region}/>;
     });
 
     return (
         <>
-        <Row>
-        <div className="search mt-2 mb-2">
-        <input type="text" value={term} onChange={handleChange} onKeyUp={handleKeyUp} />
-        <Button variant="primary" onClick={handleClick}>Search</Button>
-        </div>
-        <div>
-
-        </div>
-        </Row>
+        
+      
         <Row className= "g-2" md={3} xs={1} >
         {countryCards}
         </Row>
