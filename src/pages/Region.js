51e1480e10b5f018b47d77c97d74a1850ee85be8
Kacper Agatowski <n00212272@iadt.ie>
2023-11-02 14:01:17 +0000

    created a region page to display regions

---
+++ src/pages/Region.js
@@ -0,0 +1,10 @@
+import {useState , useEffect} from 'react';
+import axios from 'axios';
+import CountryCard from '../components/CountryCard';
+import {Row, Button} from 'react-bootstrap';
+
+const Region = (props) => {
+    
+}
+
+export default Region;
