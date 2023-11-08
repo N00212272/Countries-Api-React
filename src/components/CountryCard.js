0f58ea65264c587c24db11f6edd1fb9f1866f0b4
Kacper Agatowski <n00212272@iadt.ie>
2023-11-08 18:55:06 +0000

    changed width of cards

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -3,8 +3,8 @@ import {Link} from 'react-router-dom';
 
 const CountryCard = (props) => {
     return (
-        <Card className='m-2' style={{ width: '25rem' }}>
-            <Card.Img className="border border-black" src={props.flag} variant="top" height="250" width="250"/>
+        <Card className='m-2 border border-2' style={{ width: '25rem' }}>
+            <Card.Img className="border border-1 border-black" src={props.flag} variant="top" height="250" width="250"/>
             <Card.Body>
                 <Card.Title><Link to={`/country/${props.name}`}>{props.name}</Link></Card.Title>
                 <p>{props.region}</p>
