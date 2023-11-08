47f3a8fa97dba3c2b6b9de7716eb1a6a77c39f99
Kacper Agatowski <n00212272@iadt.ie>
2023-11-08 15:44:06 +0000

    added styling

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 
 const CountryCard = (props) => {
     return (
-        <Card>
+        <Card className='m-2' style={{ width: '25rem' }}>
             <Card.Img className="border border-black" src={props.flag} variant="top" height="250" width="250"/>
             <Card.Body>
                 <Card.Title><Link to={`/country/${props.name}`}>{props.name}</Link></Card.Title>
