bb85f278302f7938a6fd52ebccc39242659c134a
Kacper Agatowski <n00212272@iadt.ie>
2023-11-08 10:59:14 +0000

    created a regions comp

--- src/pages/Region.js
+++ src/pages/Region.js
@@ -1,10 +1,36 @@
 import {useState , useEffect} from 'react';
 import axios from 'axios';
+import { useParams } from "react-router-dom";
 import CountryCard from '../components/CountryCard';
-import {Row, Button} from 'react-bootstrap';
+import {Row} from 'react-bootstrap';
 
 const Region = (props) => {
+    let { region } = useParams();
     
+    const [countries, setCountries] = useState([])
+
+    useEffect(() => {
+        axios
+          .get(`https://restcountries.com/v3.1/region/${region}`)
+          .then((response) => {
+            console.log(response.data);
+            setCountries(response.data);
+            }) .catch(error => {
+            console.error(error);
+        });
+          
+      }, [region]);
+    
+      let countryCards =countries.map((country, i) => (
+        <CountryCard key={i} flag={country.flags.png} name={country.name.common} region={country.subregion} />
+      ))
+    return(
+        
+    
+    <Row className= "g-2" md={3} xs={1} >
+    {countryCards}
+    </Row>
+  );
 }
 
 export default Region;
