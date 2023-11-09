072587df72fac2225a94624492e4815a9ab6301f
Kacper Agatowski <n00212272@iadt.ie>
2023-11-09 18:47:30 +0000

    added fixed width

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -6,7 +6,7 @@ const CountryCard = (props) => {
 
 
     return (
-        <Card className='m-2 border border-2 border-success text-center' style={{ width: '25rem' }}>
+        <Card className='m-2 border border-2 border-success text-center ' style={{ width: '25rem' }}>
             <Card.Img className="border border-1 border-black w-100 h-100" src={props.flag} variant="top" />
             <Card.Body>
                 <Card.Title>{props.name}</Card.Title>
