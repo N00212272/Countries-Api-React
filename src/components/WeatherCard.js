bf73631ffaf02446c95bbaea410f7b18c4b42887
Kacper Agatowski <n00212272@iadt.ie>
2023-11-09 18:48:37 +0000

    changed weathercard

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,5 +1,6 @@
 import {useState , useEffect} from 'react';
 import axios from 'axios';
+import {Row,Col, Container} from 'react-bootstrap';
 
 import {Card} from 'react-bootstrap';
 
@@ -21,16 +22,27 @@ const WeatherCard = (props) => {
         })
       })
     return (
-        <Card className='rounded'>
+        <Card className='rounded border border-4 border-success' >
              {weather?.current?.condition && (
                 <Card.Body>
-                    <Card.Img className="border border-black" src={weather.current.condition.icon} variant="top" height="200"/>
-            <Card.Title><b>Current Condition</b>{weather.current.condition.text}</Card.Title>
+                  <Row>
+                    <Col>
+                   <Card.Img className="border border-black w-100" src={weather.current.condition.icon} variant="top"/>
+                   </Col>
+                   <Col>
+                  <Card.Title className=''><b>Current Condition: </b>{weather.current.condition.text}</Card.Title>
+                   
             <p><b>Current temp: </b>{weather.current.temp_c} Celcius</p>
+            <p><b>Feels like: </b>{weather.current.feelslike_c} Celcius</p>
             <p><b>Wind Speed: </b>{weather.current.wind_kph} Kp/h</p>
+            <p><b>Wind direction: </b>{weather.current.wind_dir}</p>
             <p><b>Uv index: </b>{weather.current.uv}</p>
             <p><b>Current Date and Time: </b>{weather.location.localtime}</p>
+            
+            </Col>
+            </Row>
             </Card.Body>
+            
             )}
 
         </Card>
